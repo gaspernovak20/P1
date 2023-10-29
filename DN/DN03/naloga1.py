0486e7b45a4577ed3cdec9e2de22f7bdf662bd41
gaspernovak20 <gaspernovak22@gmail.com>
2023-10-29 18:39:47 +0100

    V2 finnish, DN03 changes

--- DN/DN03/naloga1.py
+++ DN/DN03/naloga1.py
@@ -1,40 +1,25 @@
-prej = [
-            "..............##...",
-            "..###.....###....##",
-            "...###...###...#...",
-            "...........#.....##",
-            "...................",
-            "###.....#####...###"
-        ]
-
-potem = [
-            "...##..............",
-            "..........###....##",
-            "#..###...###...#...",
-            "...###.....#.....##",
-            "................###",
-            "###.....##.##...###"
-        ]
 def dolzina_ovir(vrstica):
     st_ovir = 0
     for c in vrstica:
         if c == "#":
             st_ovir += 1
 
-    return st_ovir 
+    return st_ovir
+
 
 def stevilo_ovir(vrstica):
     st_ovir = 0
     znana_ovira = False
     for c in vrstica:
         if c == "#" and not znana_ovira:
-            st_ovir +=1
+            st_ovir += 1
             znana_ovira = True
-        
+
         if c == "." and znana_ovira:
-            znana_ovira = False  
+            znana_ovira = False
 
     return st_ovir
+
 
 def najsirsa_ovira(vrstica):
     len_najsirse_ovira = 0
@@ -53,46 +38,48 @@ def najsirsa_ovira(vrstica):
         # kontrola dolzine ovire
         if c == "#" and znana_ovira:
             len_ovira += 1
-        
+
         # konec ovire
-        if (c == "." and znana_ovira) or i == (len(vrstica)-1):
+        if (c == "." and znana_ovira) or i == (len(vrstica) - 1):
             # previrimo ali je prejsnja ovira najdaljsa znana ovira do sedaj
             if len_najsirse_ovira < len_ovira:
                 len_najsirse_ovira = len_ovira
-            znana_ovira = False  
-        
+            znana_ovira = False
+
     return len_najsirse_ovira
+
 
 def pretvori_vrstico(vrstica):
     seznam_parov = []
     zacetek_ovire = 0
-    konec_ovire = 0
 
     znana_ovira = False
     for i, c in enumerate(vrstica):
 
         if c == "#" and not znana_ovira:
-            zacetek_ovire = i+1
+            zacetek_ovire = i + 1
             znana_ovira = True
-        
+
         if c == "." and znana_ovira:
             konec_ovire = i
             seznam_parov.append((zacetek_ovire, konec_ovire))
             znana_ovira = False
 
-        if znana_ovira and i == len(vrstica)-1:
-            konec_ovire = i+1
+        if znana_ovira and i == len(vrstica) - 1:
+            konec_ovire = i + 1
             seznam_parov.append((zacetek_ovire, konec_ovire))
-    
+
     return seznam_parov
+
 
 def pretvori_zemljevid(vrstice):
     seznam_ovir = []
     for i, vrstica in enumerate(vrstice):
         for ovira_kordinate in pretvori_vrstico(vrstica):
-            seznam_ovir.append(ovira_kordinate+(i+1,))
+            seznam_ovir.append(ovira_kordinate + (i + 1,))
 
     return seznam_ovir
+
 
 def izboljsave(prej, potem):
     ovire_prej = pretvori_zemljevid(prej)
@@ -108,6 +95,7 @@ def izboljsave(prej, potem):
 
     return nove_ovire
 
+
 def huligani(prej, potem):
     nove_ovire = izboljsave(prej, potem)
 
@@ -115,6 +103,23 @@ def huligani(prej, potem):
 
     return nove_ovire, ukradene_ovire
 
-dodane , ukradene = huligani(prej,potem)
-print(dodane)
-print(ukradene)
+
+prej = [
+            "..............##...",
+            "..###.....###....##",
+            "...###...###...#...",
+            "...........#.....##",
+            "...................",
+            "###.....#####...###"
+        ]
+
+potem = [
+            "...##.........##...",
+            "..###.....###....##",
+            "#..###...###...#...",
+            "...###.....#.....##",
+            "................###",
+            "###.....#####...###"
+        ]
+
+izboljsave(prej, potem)
--- VAJE/V2/najvecji.py
+++ VAJE/V2/najvecji.py
@@ -1,33 +1,35 @@
-import numpy as np
-import time
-    
+
 def naj(seznam):
     return max(seznam)
 
 
 def naj_abs(seznam):
-    return(abs(max(seznam, key=abs)))
-    
+    return abs(max(seznam, key=abs))
+
+
 def ostevilci(xs):
     ostevilcen_seznam = []
     for i, x in enumerate(xs):
         ostevilcen_seznam.append((i, x))
     print(ostevilcen_seznam)
 
+
 def bmi(oseba):
     oseba_bmi = []
     for ime, teza, visina in oseba:
-        oseba_bmi.extend((ime, teza / pow(visina/100 ,2) ))
+        oseba_bmi.extend((ime, teza / pow(visina/100, 2)))
     print(oseba_bmi) 
+
 
 def bmi2(imena, teze, visine):
     oseba_zip = zip(imena, teze, visine)
     bmi(oseba_zip)
 
+
 def prastevila(meja):
     st_prastevil = 0
-    for st in range(2,meja):
-        for x in range(2 , int(st**0.5) + 1):
+    for st in range(2, meja):
+        for x in range(2, int(st**0.5) + 1):
             if st % x == 0:
                 break
         else:
@@ -37,10 +39,28 @@ def prastevila(meja):
 
 
 def palindrom(niz):
-    if list(niz) == list(reversed(niz)): 
-        print(True)
-    else:
-        print(False)
+    return list(niz) == list(reversed(niz))
+
+
+def palindromska_stevila():
+    naj_palindrom = 0
+    for x in range(1000, 99, -1):
+        for y in range(x, 99, -1):
+            if x*y <= naj_palindrom:
+                break
+
+            if naj_palindrom < (x*y) and palindrom(str(x*y)):
+                naj_palindrom = x*y
+    print(naj_palindrom)
+
+def inverzije(xs):
+    num_inverzij = 0
+    for i, num in enumerate(xs):
+        for i_after_num in range(i, len(xs)):
+            if num > xs[i_after_num]:
+                num_inverzij += 1
+    print(num_inverzij)
+
 
 stevila = [5, 1, -6, -7, 2]
 
@@ -55,10 +75,8 @@ print(naj_abs(stevila))
 ostevilci([5, 1, 4, 2, 3])
 bmi(osebe)
 bmi2(imena, teze, visine)
-prastevila(1000000)
+prastevila(1000)
 palindrom('pericarezeracirep')
 palindrom('perica')
-
-
-
-
+palindromska_stevila()
+inverzije([1, 4, 3, 5, 2, 2, 4, 5])
